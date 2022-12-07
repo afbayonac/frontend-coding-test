9a3d59bd89302249f4bf33c114fb498a359a375b
aratoca <afbayonac@gmail.com>
2022-12-07 09:20:21 -0500

    Implement sort

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,37 @@
 import Link from 'next/link'
 import Layout from '../components/Layout/Layout'
+import { useState } from 'react'
 
 // TODO: add message when profiles are empty
 // TODO: catch server side errors
 
 function Home ({ profiles = [] }) {
+  const [sort, setSort] = useState('asc')
+
+  function toggleSort () {
+    setSort(sort === 'asc' ? 'desc' : 'asc')
+  }
+
   return (
     <Layout>
+      <div onClick={toggleSort}>
+        Age {sort === 'asc' ? '↓' : '↑'}
+      </div>
       <ol>
-        {profiles.map(profile => (
-          <li key={profile.id}>
-            <Link href={`profile/${profile.id}`}>
-              <div>
-                {profile.fullName} - {profile.age}
-              </div>
-            </Link>
-          </li>
-        ))}
+        {profiles
+          .sort((a, b) => sort === 'asc'
+            ? a.age - b.age
+            : b.age - a.age
+          )
+          .map(profile => (
+            <li key={profile.id}>
+              <Link href={`profile/${profile.id}`}>
+                <div>
+                  {profile.fullName} - {profile.age}
+                </div>
+              </Link>
+            </li>
+          ))}
       </ol>
     </Layout>
   )
